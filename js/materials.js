a795eb134d138a428364be892a2abedcbf189dad
izielin <misiopar@wp.pl>
2018-06-14 16:48:03 +0200

    zmiana wyglądów przycików i rozwinięcie strony materiały

--- js/materials.js
+++ js/materials.js
@@ -1,12 +1,12 @@
 // funkcja do edytora tekstu
-function myFunction() {
+function wordFunction() {
 var x = document.getElementById("text_editor");
   if (x.style.visibility === "visible") {
     x.style.visibility = "hidden";
     } else {
     x.style.visibility = "visible";
-    }
-    document.querySelector('#text_editor').scrollIntoView({ 
-  behavior: 'smooth'
-});
+    document.querySelector('#text_editor').scrollIntoView({
+      behavior: 'smooth'
+    });
   }
+}
