cacdac11aa66351343b4516b5c87d60fb80c96c8
pwojton <piotr.wojton.01@gmail.com>
2018-07-16 15:16:34 +0200

    dodanie biblioteki moveit

---
+++ js/hamburger.js
@@ -0,0 +1,11 @@
+Moveit.put(circle, {  //funkcja odpowiadająca za rozpoczęcie i zakończenie rysowania koła
+  start: '0%',
+  end: '14%'
+});
+// Moveit.animate(circle, {
+//   start: '0%',
+//   end: '100%',
+//   duration: 0.6,
+//   delay: 0,
+//   timing: 'ease-out'
+// });
