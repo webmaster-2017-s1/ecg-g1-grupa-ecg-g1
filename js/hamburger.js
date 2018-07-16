3118e83d205d52ec385954d19d8f0ed9ab928696
pwojton <piotr.wojton.01@gmail.com>
2018-07-16 16:07:20 +0200

    dodanie animowanego hamburgera

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,11 +1,81 @@
-Moveit.put(circle, {  //funkcja odpowiadająca za rozpoczęcie i zakończenie rysowania koła
-  start: '0%',
-  end: '14%'
-});
-// Moveit.animate(circle, {
-//   start: '0%',
-//   end: '100%',
-//   duration: 0.6,
-//   delay: 0,
-//   timing: 'ease-out'
-// });
+$(function() {
+    var open = false,
+        duration = 0.9,
+        timing = 'cubic-bezier(0.7, 0, 0.3, 1)';
+    Moveit.put(first, {
+        start: '0%',
+        end: '14%',
+        visibility: 1
+    });
+    Moveit.put(second, {
+        start: '0%',
+        end: '11.5%',
+        visibility: 1
+    });
+    Moveit.put(middle, {
+        start: '0%',
+        end: '100%',
+        visibility: 1
+    });
+    $('.trigger').click(function() {
+        if (!open) {
+            Moveit.animate(first, {
+                visibility: 1,
+                start: '78%',
+                end: '93%',
+                duration: duration,
+                delay: 0,
+                timing: timing
+            });
+            Moveit.animate(middle, {
+                visibility: 1,
+                start: '50%',
+                end: '50%',
+                duration: duration,
+                delay: 0,
+                timing: timing
+            });
+            Moveit.animate(second, {
+                visibility: 1,
+                start: '81.5%',
+                end: '94%',
+                duration: duration,
+                delay: 0,
+                timing: timing
+            });
+        } else {
+          Moveit.animate(middle, {
+                visibility: 1,
+                start: '0%',
+                end: '100%',
+                duration: duration,
+                delay: 0,
+                timing: timing
+            });
+					Moveit.animate(middle, {
+                visibility: 1,
+                duration: duration,
+                delay: 0,
+                timing: timing
+            });
+          Moveit.animate(first, {
+                visibility: 1,
+                start: '0%',
+        				end: '14%',
+                duration: duration,
+                delay: 0,
+                timing: timing
+            });
+          Moveit.animate(second, {
+                visibility: 1,
+                start: '0%',
+        				end: '11.5%',
+                duration: duration,
+                delay: 0,
+                timing: timing
+            });
+        }
+        open = !open;
+    });
+
+})
