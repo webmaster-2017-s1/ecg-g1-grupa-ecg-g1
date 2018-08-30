0e313e6c4c4af3bcecfaaf51bc70e4551875065c
zbrsnd <zbrsnd@gmail.com>
2018-08-30 15:12:21 +0200

    Poprawa archiwów

--- js/parallax.js
+++ js/parallax.js
@@ -4,11 +4,11 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop(); //podanie informacji o ile pixeli użytkownik przewinął względem góry strony
 
   $('.logo').css({
-    'transform': 'translate(0px, '+ wScroll /2 +'%)' //przesunięcie diva w dół zraz z przesuwaniem strony, /2 - powoduje spowolnienie 0 połowę, + oznaczają że przesuwaja się o wartoś dodatnią
+    'transform': 'translate(0px, '+ wScroll /5 +'%)' //przesunięcie diva w dół zraz z przesuwaniem strony, /2 - powoduje spowolnienie 0 połowę, + oznaczają że przesuwaja się o wartoś dodatnią
   });
 
   $('.third-plan').css({
-    'transform': 'translate(0px, '+ wScroll /4 +'%)'
+    'transform': 'translate(0px, '+ wScroll /3 +'%)'
   });
 
   $('.first-plan').css({
