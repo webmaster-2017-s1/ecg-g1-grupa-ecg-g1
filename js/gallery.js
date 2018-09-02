32507a200dc314964f878e624cd1a7f5ae520004
izielin <misiopar@wp.pl>
2018-09-02 17:30:20 +0200

    uzupełnienie galerii w archiwum "Bridge Linux"

---
+++ js/gallery.js
@@ -0,0 +1,119 @@
+$("#grid img").click(function(){
+  $("#imgBig").attr("src",$(this).prop('src'));
+  $("#overlay").show('slow');
+  $("#overlayContent").show('slow');
+});
+
+$("#imgBig").click(function(){
+  $("#imgBig").attr("src", "");
+  $("#overlay").hide();
+  $("#overlayContent").hide();
+});
+
+$("#grid_2 img").click(function(){
+  $("#imgBig_2").attr("src",$(this).prop('src'));
+  $("#overlay_2").show('slow');
+  $("#overlayContent_2").show('slow');
+});
+
+$("#imgBig_2").click(function(){
+  $("#imgBig_2").attr("src", "");
+  $("#overlay_2").hide();
+  $("#overlayContent_2").hide();
+});
+
+$("#grid_3 img").click(function(){
+  $("#imgBig_3").attr("src",$(this).prop('src'));
+  $("#overlay_3").show('slow');
+  $("#overlayContent_3").show('slow');
+});
+
+$("#imgBig_3").click(function(){
+  $("#imgBig_3").attr("src", "");
+  $("#overlay_3").hide();
+  $("#overlayContent_3").hide();
+});
+
+$("#grid_4 img").click(function(){
+  $("#imgBig_4").attr("src",$(this).prop('src'));
+  $("#overlay_4").show('slow');
+  $("#overlayContent_4").show('slow');
+});
+
+$("#imgBig_4").click(function(){
+  $("#imgBig_4").attr("src", "");
+  $("#overlay_4").hide();
+  $("#overlayContent_4").hide();
+});
+
+$("#grid_5 img").click(function(){
+  $("#imgBig_5").attr("src",$(this).prop('src'));
+  $("#overlay_5").show('slow');
+  $("#overlayContent_5").show('slow');
+});
+
+$("#imgBig_5").click(function(){
+  $("#imgBig_5").attr("src", "");
+  $("#overlay_5").hide();
+  $("#overlayContent_5").hide();
+});
+
+$("#grid_6 img").click(function(){
+  $("#imgBig_6").attr("src",$(this).prop('src'));
+  $("#overlay_6").show('slow');
+  $("#overlayContent_6").show('slow');
+});
+
+$("#imgBig_6").click(function(){
+  $("#imgBig_6").attr("src", "");
+  $("#overlay_6").hide();
+  $("#overlayContent_6").hide();
+});
+
+$("#grid_7 img").click(function(){
+  $("#imgBig_7").attr("src",$(this).prop('src'));
+  $("#overlay_7").show('slow');
+  $("#overlayContent_7").show('slow');
+});
+
+$("#imgBig_7").click(function(){
+  $("#imgBig_7").attr("src", "");
+  $("#overlay_7").hide();
+  $("#overlayContent_7").hide();
+});
+
+$("#grid_8 img").click(function(){
+  $("#imgBig_8").attr("src",$(this).prop('src'));
+  $("#overlay_8").show('slow');
+  $("#overlayContent_8").show('slow');
+});
+
+$("#imgBig_8").click(function(){
+  $("#imgBig_8").attr("src", "");
+  $("#overlay_8").hide();
+  $("#overlayContent_8").hide();
+});
+
+$("#grid_9 img").click(function(){
+  $("#imgBig_9").attr("src",$(this).prop('src'));
+  $("#overlay_9").show('slow');
+  $("#overlayContent_9").show('slow');
+});
+
+$("#imgBig_9").click(function(){
+  $("#imgBig_9").attr("src", "");
+  $("#overlay_9").hide();
+  $("#overlayContent_9").hide();
+});
+
+$("#grid_10 img").click(function(){
+  $("#imgBig_10").attr("src",$(this).prop('src'));
+  $("#overlay_10").show('slow');
+  $("#overlayContent_10").show('slow');
+});
+
+$("#imgBig_10").click(function(){
+  $("#imgBig_10").attr("src", "");
+  $("#overlay_10").hide();
+  $("#overlayContent_10").hide();
+});
